fc554737db67cfc31e26117ef9f20e2144f46d76
sk18aeq <sk18aeq@ENT-D410-34.herts.ac.uk>
2019-04-08 13:10:10 +0100

    my master commit

---
+++ test/embeddedmediaplayer/ClipTest.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//master Branch 1
+package embeddedmediaplayer;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author comqsjb
+ */
+public class ClipTest {
+    
+    public ClipTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getTitle method, of class Clip.
+     */
+    @Test
+    public void testSetTitleToEmptyStringKeepsPreviousValue()
+    {
+    }
+
+    @Test
+    public void testSetEndBeforeStartKeepsPreviousValue()
+    {
+    }
+
+    @Test
+    public void testEqualsOnEqualClips() 
+    {
+    }
+    
+    @Test
+    public void testEqualsOnNonEqualClips() 
+    {
+    }
+    
+    @Test
+    public void testSetEndToNegativeNumberKeepsPreviousValue() 
+    {
+    }
+    
+    @Test
+    public void testSetStartToValidPositiveNumber() 
+    {    
+    }
+    
+    
+    
+}
