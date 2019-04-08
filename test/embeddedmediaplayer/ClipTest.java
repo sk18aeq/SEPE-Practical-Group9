09015418924c032ffd3d30938421bba7d96ef99f
sk18aeq <sk18aeq@ENT-D410-34.herts.ac.uk>
2019-04-08 13:19:50 +0100

    1st test case

--- test/embeddedmediaplayer/ClipTest.java
+++ test/embeddedmediaplayer/ClipTest.java
@@ -45,16 +45,31 @@ public class ClipTest {
     @Test
     public void testSetTitleToEmptyStringKeepsPreviousValue()
     {
+       System.out.println("SetTitle");
+        Clip clip = new Clip();
+        String InitialTitle = "First Title";
+        clip.setTitle(InitialTitle); // Set title as First Title to the clip  
+        // try to set Empty Title
+        String EmptyTitle = "";
+        clip.setTitle(EmptyTitle);              
+        // check title is modified
+        String ModifiedTitle = clip.getTitle(); // check Empty Title is set or not 
+        // Compare  Initial title and resulted title
+        assertTrue(InitialTitle.equals(ModifiedTitle));
+        System.out.println("Empty Title is not accepted");     
+        
     }
 
     @Test
     public void testSetEndBeforeStartKeepsPreviousValue()
     {
+       
     }
 
     @Test
     public void testEqualsOnEqualClips() 
     {
+       
     }
     
     @Test
